b2179a1268a0834841e5f6872f735260311bfc57
Henrique Cervi <henrique.cervi@terra.com.br>
2021-06-24 20:59:03 -0300

    VCriando UsuarioPojo, encapsulamento.

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -2,8 +2,11 @@ package modulos.produto;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.UsuarioPojo;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -11,34 +14,40 @@ import static org.hamcrest.Matchers.*;
 @DisplayName("Testes de API Rest do módulo de Produto")
 public class ProdutoTest {
 
-    @Test
-    @DisplayName("Validar os limites proibidos do valor do Produto")
-    public void testValidarLimitesProibidosValorProduto() {
+    private String token;
 
+    @BeforeEach
+    public void baseUriEToken() {
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
         // port = 8080;
         basePath = "/lojinha";
 
-        // Obter o token do usuário admin
-        String token = given()
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "  \"usuarioLogin\": \"admin\",\n" +
-                        "  \"usuarioSenha\": \"admin\"\n" +
-                        "}")
-            .when()
-                .post("/v2/login")
-            .then()
-                .extract()
-                    .path("data.token");
+        UsuarioPojo usuario = new UsuarioPojo();
+        usuario.setUsuarioLogin("admin");
+        usuario.setUsuarioSenha("admin");
 
-         // Tentar inserir um produto com valor 0,00 e validar que a msg de erro fora apresentada e o
+        // Obter o token do usuário admin
+        this.token = given()
+                .contentType(ContentType.JSON)
+                .body(usuario)
+                .when()
+                .post("/v2/login")
+                .then()
+                .extract()
+                .path("data.token");
+    }
+
+    @Test
+    @DisplayName("Validar o limite zerado do valor do Produto")
+    public void testValidarLimiteZeradoValorProduto() {
+
+        // Tentar inserir um produto com valor 0,00 e validar que a msg de erro fora apresentada e o
         // status code retornado foi 422
 
         given()
                 .contentType(ContentType.JSON)
-                .header("token", token)
+                .header("token", this.token)
                 .body("{\n" +
                         "  \"produtoNome\": \"PlayStation 6\",\n" +
                         "  \"produtoValor\": 0.00,\n" +
@@ -53,9 +62,39 @@ public class ProdutoTest {
                         "    }\n" +
                         "  ]\n" +
                         "}")
-        .when()
+                .when()
                 .post("/v2/produtos")
-        .then()
+                .then()
+                .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+
+    }
+
+    @Test
+    @DisplayName("Validar o limite maior que sete mil do valor do Produto")
+    public void testValidarLimiteMaiorSeteMilValorProduto() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"PlayStation 6\",\n" +
+                        "  \"produtoValor\": 7000.01,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"Controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+                .when()
+                .post("/v2/produtos")
+                .then()
                 .assertThat()
                 .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
                 .statusCode(422);
